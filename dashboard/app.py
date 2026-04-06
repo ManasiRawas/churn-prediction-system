7a8af1845b48e3fc5683689ae35549e69d1c2771
Manasi Rawas <71628834+ManasiRawas@users.noreply.github.com>
2026-04-06 12:52:46 +0530

    Add Streamlit dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,28 @@
 import streamlit as st
+import pandas as pd
+import joblib
+
+# Load model
+model = joblib.load("model.pkl")
 
 st.title("Customer Churn Prediction")
 
-st.write("This is your ML app UI 🚀")
+# Input fields
+tenure = st.number_input("Tenure", min_value=0)
+MonthlyCharges = st.number_input("Monthly Charges", min_value=0.0)
+TotalCharges = st.number_input("Total Charges", min_value=0.0)
+
+# Predict button
+if st.button("Predict"):
+    input_data = pd.DataFrame([{
+        "tenure": tenure,
+        "MonthlyCharges": MonthlyCharges,
+        "TotalCharges": TotalCharges
+    }])
+
+    prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.error("Customer will churn ❌")
+    else:
+        st.success("Customer will stay ✅")
