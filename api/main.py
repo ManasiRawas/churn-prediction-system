979ce97297cae68fda6f5e73378428ed72d560dd
Manasi Rawas <71628834+ManasiRawas@users.noreply.github.com>
2026-04-06 12:50:56 +0530

    Add FastAPI for churn prediction

--- api/main.py
+++ api/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import pandas as pd
+import joblib
 
 app = FastAPI()
 
+# Load model
+model = joblib.load("model.pkl")
+
 @app.get("/")
 def home():
-    return {"message": "API is running 🚀"}
+    return {"message": "Churn Prediction API is running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    df = pd.DataFrame([data])
+    prediction = model.predict(df)
+    return {"prediction": int(prediction[0])}
